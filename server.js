52e12f538e43e006b3cef20ca2d5ac5e67be56d7
bdurham227 <bdurham227@gmail.com>
2021-07-19 21:14:54 -0400

    reworked package.json and added my github urls to it, added .env file and process.env

--- server.js
+++ server.js
@@ -15,6 +15,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+
+const { MongoClient } = require('mongodb');
+const uri = process.env.MONGO_DB_URI || "mongodb+srv://admin:password12345@cluster0.49a2f.mongodb.net/Workout-Tracker?retryWrites=true&w=majority";
+const client = new MongoClient(uri, {useNewUrlParser: true, useUnifiedTopology: true})
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  client.close();
+});
+
+
+
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false,
